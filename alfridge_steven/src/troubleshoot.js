4865c5382a95bb580f5f9f885ede5d69de8f727f
stevenwalentino <stevenwalentino@yahoo.com>
2015-04-10 14:09:31 -0700

    Grocery Lists and Items

---
+++ alfridge_steven/src/troubleshoot.js
@@ -0,0 +1,8 @@
+
+var whiteSkin = new Skin({fill:"white"});
+var titleStyle = new Style({font:"20px", color:"black"}); 
+var troubleshootMainBody =  new Container({top:40, bottom:0, right: 0, left:0, skin:whiteSkin, 
+			contents:[
+				new Label({left:0, right:0, top:10,  string: "Troubleshoot", style: titleStyle }),	], 	
+		});
+					
