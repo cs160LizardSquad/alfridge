976a7660bcd5dc9d23c7df149a09bab471c2a7d6
Diane <dianecwang@gmail.com>
2015-04-16 11:21:59 -0700

    interactive prototype checkpoint

--- alfridge/src/troubleshoot.js
+++ alfridge/src/troubleshoot.js
@@ -1,7 +1,7 @@
 
 var whiteSkin = new Skin({fill:"white"});
 var titleStyle = new Style({font:"20px", color:"black"}); 
-var troubleshootMainBody =  new Container({top:40, bottom:0, right: 0, left:0, skin:whiteSkin, 
+var troubleshootMainBody =  new Container({top:50, bottom:0, right: 0, left:0, skin:whiteSkin, 
 			contents:[
 				new Label({left:0, right:0, top:10,  string: "Troubleshoot", style: titleStyle }),	], 	
 		});
