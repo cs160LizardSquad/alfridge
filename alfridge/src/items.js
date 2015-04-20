5f7dd492ac5518dfcd251f40a43ef41363baa013
peuanthinsan <peuanthinsan@berkeley.edu>
2015-04-19 20:50:38 -0700

    grocery lists front and back

--- alfridge/src/items.js
+++ alfridge/src/items.js
@@ -1,6 +1,20 @@
-var whiteSkin = new Skin({fill:"white"});
-var titleStyle = new Style({font:"20px", color:"black"}); 
-var itemsMainBody =  new Container({top:50, bottom:0, right: 0, left:0, skin:whiteSkin, 
+var itemsColumn = new Column({left: 0, right: 0, top: 100, bottom: 0, width: application.width, height: application.height, skin: lightgreySkin, style: textStyle,
+	contents: [
+	]
+});
+
+function updateItemsBody(allItemsDict){
+	var data = {};
+	for(var item in JSON.parse(allItemsDict)){
+		trace(item);
+		data = allItemsDict[item];
+		itemsColumn.add(new Label({left: 0, right: 0, top: 0, bottom: 0, string: data.name}));
+		
+	}
+}
+
+var itemsMainBody = new Container({top:50, bottom:0, skin:whiteSkin, 
 			contents:[
-				new Picture({left:-124, top: 0, height: 489, width: 570, url: "assets/items.png"}),	], 	
+				itemsColumn
+			], 	
 		});
